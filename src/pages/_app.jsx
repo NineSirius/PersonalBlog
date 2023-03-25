0192bfd602dfb9207fb1a66ad937873f1a6e7255
Ruslan Bulah <ruslanbulah3@gmail.com>
2023-03-25 11:08:04 +0600

    start project

---
+++ src/pages/_app.jsx
@@ -0,0 +1,19 @@
+import { Layout } from '@/containers/Layout'
+import '@/styles/globals.css'
+
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+    weight: ['400', '500', '600', '700'],
+    subsets: ['latin'],
+})
+
+export default function App({ Component, pageProps }) {
+    return (
+        <div className={montserrat.className}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </div>
+    )
+}
